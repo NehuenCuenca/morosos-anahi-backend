24e180beaeba04fa595204d1712e96c63b42552e
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-11-28 12:04:22 -0300

    Append unit_price_quantity_detail new attribute to load into the defaulter form from the FE

--- app/Models/Debt.php
+++ app/Models/Debt.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class Debt extends Pivot
 {
@@ -32,4 +34,23 @@ class Debt extends Pivot
         'retired_at' => 'datetime:Y-m-d\TH:i',
         'filed_at' => 'datetime:Y-m-d\TH:i'
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['unit_price_quantity_detail'];
+
+    /**
+     * Load for the UnitPriceQuantityDetail input on the FE
+     */
+    protected function unitPriceQuantityDetail(): Attribute
+    {
+        $detail = Thing::find($this->thing_id)->name;
+
+        return Attribute::make(
+            get: fn() => "{$this->unit_price}|{$this->quantity}|{$detail}"
+        );
+    }
 }
